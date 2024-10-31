bce36515257bfc02ef22ce14a4f14e04a9e12ae3
Begis Orinbaev <begisorynbaev@gmail.com>
2024-10-31 15:47:07 +0500

    added react toastify

--- src/components/ToDoList/ToDoListItem/ToDoListItem.tsx
+++ src/components/ToDoList/ToDoListItem/ToDoListItem.tsx
@@ -7,18 +7,20 @@ export const ToDoListItem = (props: {
   deleteToDo: Function;
 }) => {
   return (
-    <li className="todo-list-item__wrapper">
-      <span>{props.toDoItem.text}</span>
-      <div className="todo-list-item__buttons">
-        <button
-          className="btn-trash"
-          onClick={() => props.deleteToDo(props.toDoItem)}
-        ></button>
-        <button
-          className={props.toDoItem.isDone ? "btn-check" : "btn-uncheck"}
-          onClick={() => props.updateToDo(props.toDoItem)}
-        ></button>
-      </div>
-    </li>
+    <>
+      <li className="todo-list-item__wrapper">
+        <span>{props.toDoItem.text}</span>
+        <div className="todo-list-item__buttons">
+          <button
+            className="btn-trash"
+            onClick={() => props.deleteToDo(props.toDoItem)}
+          ></button>
+          <button
+            className={props.toDoItem.isDone ? "btn-check" : "btn-uncheck"}
+            onClick={() => props.updateToDo(props.toDoItem)}
+          ></button>
+        </div>
+      </li>
+    </>
   );
 };
--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -3,6 +3,8 @@ import { Form } from "../components/Form/Form";
 import { Header } from "../components/Header/Header";
 import { ToDoList } from "../components/ToDoList/ToDoList";
 import { ToDo } from "../models/todo-item";
+import { Bounce, Flip, toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const ToDoListPage = () => {
   const [todos, setTodos] = useState<ToDo[]>([]);
@@ -15,27 +17,79 @@ export const ToDoListPage = () => {
       isDone: false,
     };
     setTodos([...todos, newToDo]);
+
+    toast.info("Задача создана!", {
+      position: "top-left",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Flip,
+    });
   };
 
   const updateToDo = (toDoItem: ToDo) => {
-    const newTodos = todos.map((todo)=> {
+    const newTodos = todos.map((todo) => {
       if (todo.id === toDoItem.id) {
-        todo.isDone = !todo.isDone
+        todo.isDone = !todo.isDone;
+        if (todo.isDone) {
+          toast.success("Задача выполнена!", {
+            position: "top-left",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Flip,
+          });;
+        }
+      } else {
+        
       }
-      return todo
-    })
-    setTodos(newTodos)
+      return todo;
+    });
+    setTodos(newTodos);
+    
   };
 
   const deleteToDo = (toDoItem: ToDo) => {
-    const newTodos = todos.filter((todo) => todo.id !== toDoItem.id)
-    setTodos(newTodos)
+    const newTodos = todos.filter((todo) => todo.id !== toDoItem.id);
+    setTodos(newTodos);
+    toast.error('Задача удалена', {
+      position: "top-left",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Flip,
+      });
   };
   return (
     <>
       <Header />
       <Form createNewTodo={createNewTodo} />
-      <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo}/>
+      <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
+      <ToastContainer
+        position="top-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Flip}
+      />
     </>
   );
 };
