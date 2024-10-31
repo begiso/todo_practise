e3b91cb7a74c82b81225ff9e6abc49e043dfe84c
Begis Orinbaev <begisorynbaev@gmail.com>
2024-10-31 15:16:22 +0500

    add logic for todo

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,11 +1,25 @@
-import './Form.css'
+import { useState } from "react";
+import "./Form.css";
 
-export const Form = () => {
+export const Form = (props: { createNewTodo: Function }) => {
+  const [text, setText] = useState<string>("");
+
+  const formSubmit = () => {
+    if (text) {
+      props.createNewTodo(text);
+      setText("");
+    }
+  };
+
   return (
     <div className="form-wrapper">
-      <form action="#">
+      <form action="#" onSubmit={formSubmit}>
         <label>
-          <input type="text" />
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
           <button></button>
         </label>
       </form>
